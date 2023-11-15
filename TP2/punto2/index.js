1efa8a61ad7a9f48a0135c2d83bce0ef27a1f354
NicolasCaceres99 <nicoca9908@gmail.com>
2023-11-15 08:44:59 -0300

    cambios punto 2

--- TP2/punto2/index.js
+++ TP2/punto2/index.js
@@ -36,4 +36,55 @@ if (!isNaN(nota) && nota >= 0 && nota <= 10 ){
     alert('calificacion ' + calificacion);
 }else {
     document.write('introduce un numero valido');
+
 }
+
+
+// 2- Escribir un programa que solicite una nota (número) de 0  a 10. Luego mostrar la calificación en un alert según los siguientes rangos de nota:
+
+// 0-2: Muy deficiente
+// 3-4: Insuficiente
+// 5-6: Suficiente
+// 7: Bien
+// 8-9: Notable
+// 10: Sobresaliente
+
+// Si ingreso un número que no esté dentro del rango de 0 a 10 mostrar un mensaje de “número erróneo”. Si el número ingresado no es válido mostrar el mensaje “Introduce un número válido”.
+
+// Ejemplo:
+
+
+
+// Input: 5
+// Input: 50
+// Input: hola10
+// Output: Suficiente
+// Output: Número erróneo
+// Output: Introduce un número válido
+// let nota = parseInt(prompt('ingrese la nota'))
+
+// if(nota == 0 || nota == 2){
+//     document.write('muy deficiente')
+// }
+// else if ( nota == 3   || nota == 4 ){
+//     document.write('insuficiente')
+// }
+// else if (nota == 5  ||  nota == 6){
+//     document.write("suficiente")
+// }
+// else if (nota == 7){
+//     document.write("bien")
+// }
+// else if (nota ==8  ||  nota ==9 ){
+//     document.write('notable')
+
+// }
+// else if (nota == 10 ){
+//     document.write('sobresaliente')
+// }
+// else{
+//     document.write('ingrese una nota valida')
+// }
+
+
+
