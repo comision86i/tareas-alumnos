cdd9c1ac5951d0eeb8ffda7cfbc0f5949f5af08e
xN4CH0x <juarezignacio41c1@gmail.com>
2023-12-05 18:29:40 -0300

    Se agrega tarea 5

---
+++ tarea 5/tarea 5.js
@@ -0,0 +1,15 @@
+// Convierta este ejercicio en Arrow Function
+
+
+// function suma(a, b) {
+//     let resultado = a + b;
+//     return resultado;
+// }
+// document.write(`el resultado de la suma es ${suma(3, 7)}`);
+
+
+let suma = (a, b) => {
+    let resultado = a + b;
+    return resultado;
+}
+document.write(`el resultado de la suma es ${suma(3, 7)}`);
