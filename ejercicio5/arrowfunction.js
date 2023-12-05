3a0a5f18fee6c125aebbccea7019c833f343754a
tomichiruss <lukadayo03@gmail.com>
2023-12-05 20:52:57 -0300

    Resolucion de tareas

---
+++ ejercicio5/arrowfunction.js
@@ -0,0 +1,13 @@
+/*function suma(a, b) {
+    let resultado = a + b;
+    return resultado;
+    }
+    document.write(`el resultado de la suma es ${suma(3, 7)}`); */
+
+
+let suma = (a, b) => {
+    let resultado = a + b;
+    return resultado;
+}
+
+document.write(`el resultado de la suma es ${suma(3, 7)}`);
