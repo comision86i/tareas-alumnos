8ddede9cf0082c7605d22966ed012a9be820f1c5
NicolasCaceres99 <nicoca9908@gmail.com>
2023-12-06 10:01:14 -0300

    funciones

---
+++ tareaFunciones/index.js
@@ -0,0 +1,18 @@
+let suma = function (a,b){
+    return (a+b)
+}
+console.log(suma (4,5))
+
+
+
+function suma  (a,b){
+let resultado = a+ b 
+return resultado
+}
+document.write(` elresultado es  ${suma(3,7)}` )
+
+
+
+
+let suma = (a,b) => a+b;
+document.write(suma(4,3))
